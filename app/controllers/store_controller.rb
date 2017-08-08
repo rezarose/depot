f69a4426826340ee15c052f628ca452123676135
rezarose <arrose@outlook.com>
2017-08-08 21:42:35 +0600

    Cart Creation

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,14 @@
 class StoreController < ApplicationController
   def index
+    @count = increment_count
     @products = Product.order(:title)
+  end
+
+  def increment_count
+    session[:counter] ||= 0
+    session[:counter] += 1
+    if session[:counter] >= 5
+      return session[:counter]
+    end
   end
 end
